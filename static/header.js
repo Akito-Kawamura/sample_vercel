65f8eac4a2c1ed72cfc3bc3491f5ba2e05685693
Akito_macbookpro <akito.kawamura0310@gmail.com>
2020-09-09 00:43:47 +0900

    repaired

---
+++ static/header.js
@@ -0,0 +1,8 @@
+var App = new Vue({
+  el: '#header',
+  template: `
+  <el-container>
+  <el-header>Header</el-header>
+  </el-container>
+  `
+})
--- static/main.js
+++ static/main.js
@@ -1,6 +1,6 @@
 var App = new Vue({
   el: '#app',
-  template: '
+  template: `
   <div>
   <el-container style="height: 500px; border: 1px solid #eee">
     <el-aside width="200px" style="background-color: rgb(238, 241, 246)">
@@ -52,6 +52,32 @@ var App = new Vue({
         </el-submenu>
       </el-menu>
     </el-aside>
+
+    <el-container>
+    <el-header style="text-align: right; font-size: 12px">
+      <el-dropdown>
+        <i class="el-icon-setting" style="margin-right: 15px"></i>
+        <el-dropdown-menu slot="dropdown">
+          <el-dropdown-item>View</el-dropdown-item>
+          <el-dropdown-item>Add</el-dropdown-item>
+          <el-dropdown-item>Delete</el-dropdown-item>
+        </el-dropdown-menu>
+      </el-dropdown>
+      <span>Tom</span>
+    </el-header>
+
+        <el-main>
+          <el-table :data="tableData">
+            <el-table-column prop="date" label="Date" width="140">
+            </el-table-column>
+            <el-table-column prop="name" label="Name" width="120">
+            </el-table-column>
+            <el-table-column prop="address" label="Address">
+            </el-table-column>
+          </el-table>
+        </el-main>
+      </el-container>
+    </el-container>
   </div>
 
   <style>
@@ -65,7 +91,7 @@ var App = new Vue({
     color: #333;
   }
 </style>
-  ',
+  `,
 
   data: function(){
     const item = {
